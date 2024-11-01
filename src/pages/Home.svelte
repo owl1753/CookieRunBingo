2ec05cac453fbe26cdd14feb64f47b33446b104e
owl1753 <bighammer1753@gmail.com>
2024-11-01 20:43:47 +0900

    feat: 라이센스 추가

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -13,7 +13,7 @@
         <div class="space-y-6">
             <div class="p-4 rounded-lg space-y-1">
                 <p class="text-gray-700">
-                    AI 추천을 통해 최적의 수를 찾거나, 빙고 게임을 플레이할 수 있습니다.
+                    AI 추천을 통해 최적의 수를 찾거나, 빙고를 플레이할 수 있습니다.
                 </p>
                 <p class="text-xs text-gray-400">
                     '최적 수 찾기'에서는 기본으로 가운데 칸을 선택 후 시작합니다.
@@ -21,10 +21,10 @@
             </div>
 
 
-
             <div class="text-center">
-                <button on:click={() => alert("준비 중입니다.")} class="transition bg-blue-500 active:bg-blue-900 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                    빙고 게임 플레이
+                <button on:click={() => alert("준비 중입니다.")}
+                        class="transition bg-blue-500 active:bg-blue-900 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    빙고 플레이
                 </button>
                 <a use:link href="/game">
                     <button class="transition bg-blue-500 active:bg-blue-900  hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
@@ -32,6 +32,14 @@
                     </button>
                 </a>
             </div>
+
+            <div class="text-xs text-gray-300">
+                <p>이 페이지에는 데브시스터즈에서 제작한 ‘쿠키런 글꼴’이 적용되어 있습니다.
+                    (<a class="text-gray-300 underline" href="https://www.cookierunfont.com/static/download/License_ko_en.pdf" target="_blank">라이센스</a>) <br>
+                </p>
+            </div>
+
+
         </div>
     </div>
 </main>
